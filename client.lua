bc7ea8153bf6b2f5daaf46ce65b4c1c7e1ce3e22
feelfreetofee <89666307+feelfreetofee@users.noreply.github.com>
2021-11-19 02:09:56 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -141,3 +141,21 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
+Citizen.CreateThread(function()
+    local dict = "missminuteman_1ig_2"
+
+    RequestAnimDict(dict)
+    while not HasAnimDictLoaded(dict) do
+        Citizen.Wait(100)
+    end
+    while true do
+        Citizen.Wait(0)
+        if IsControlJustPressed(1, 323) then
+                TaskPlayAnim(GetPlayerPed(-1), dict, "handsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
+        end
+        if IsControlJustReleased(1, 323) then
+                ClearPedTasks(GetPlayerPed(-1))
+        end
+    end
+end)
